ec3e37352cc0825125dc1cc352e0a72ba10eabb3
Nicolas Donna <donnanicolas@gmail.com>
2013-08-04 12:34:55 -0300

    Correct representation of MD5 string

--- tramullo.c
+++ tramullo.c
@@ -4,8 +4,13 @@
 #include "md5/md5.c"
 
 void* tramullo (void* param){
-	int readed = 0, i=0;
-	char buffer[1024], *request = NULL;
+	int readed = 0
+	  , i=0;
+	char buffer[1024]
+	   , md5[32]
+	   , aux[2]
+	   , *request = NULL;
+
 	unsigned char result[16];
 
 	int socketfd = *(int *) param;
@@ -33,11 +38,23 @@ void* tramullo (void* param){
 		MD5_Update(&ctx, buffer, readed);
 	}
 
-	MD5_Final(result, &ctx);	
+	MD5_Final(result, &ctx);
 
-	write(socketfd, result, 16);
+	for(i = 0; i < 16; i++) {		
+		sprintf(aux, "%hhx", result[i]);
+		if( ((int) strlen(aux)) > 1) { 
+			md5[(i*2)] = *(aux);
+			md5[(i*2) + 1] = *(aux + 1);
+		} else {
+			md5[(i*2)] = '0';
+			md5[(i*2) + 1] = *aux;
+		}
+	}
+
+	write(socketfd, md5, 32);
 
 	close (socketfd);
+
 	printf("Request handled\n");
 	return NULL;
 }
